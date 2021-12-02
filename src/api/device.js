bc7129e8f5bd1aaeb75c3331bc9aa25496bbcf86
iusugar <731801843@qq.com>
2021-12-02 18:55:30 +0800

    device list page update and delete

--- src/api/device.js
+++ src/api/device.js
@@ -15,7 +15,7 @@ export function checkIsExist(deviceId) {
     params: { deviceId }
   })
 }
-
+// 根据房间号获取设备
 export function fetchDevice(room) {
   return request({
     url: '/device/getRoomDevice',
@@ -23,10 +23,34 @@ export function fetchDevice(room) {
     params: { room }
   })
 }
-
+// 获取所有设备
 export function fetchAllDevice() {
   return request({
     url: '/device/getAllDevice',
     method: 'get'
+  })
+}
+// 根据搜索条件查找设备
+export function fetchDeviceList(query) {
+  return request({
+    url: '/device/getDeviceList',
+    method: 'get',
+    params: query
+  })
+}
+// 更新设备信息
+export function updateDevice(device) {
+  return request({
+    url: '/device/update',
+    method: 'put',
+    data: device
+  })
+}
+
+export function deleteDevice(device) {
+  return request({
+    url: '/device/delete',
+    method: 'delete',
+    data: device
   })
 }
