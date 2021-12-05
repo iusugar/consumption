cc429aa826d8975d0b103b12883725fde89c8151
iusugar <731801843@qq.com>
2021-12-05 13:06:09 +0800

    add more functions in location.js to request device location data

--- src/api/location.js
+++ src/api/location.js
@@ -1,9 +1,3 @@
 import request from '@/utils/request.js'
 
-export function fetchYesterdayData(devId) {
-  return request({
-    url: '/electricityData/getYesterdayData',
-    method: 'get',
-    params: { devId }
-  })
-}
+
